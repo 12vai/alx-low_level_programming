a220d8d46f7e100449a92f611a71e4a16ada2c0e
12vai <vailetchiwowa@gmail.con>
2023-03-19 11:16:03 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,11 @@
  */
 int main(void)
 {
-	printf("Size); of a char : '%lu' byte(s)\'n", sizeof(long unsigned int));
-	printf("Size); of an int : '%lu' byte(s)\'n", sizeof(long unsigned int));
-	printf("Size); of a long int : '%lu' byte(s)\'n", sizeof(long unsigned int));
-	printf("Size); of a long long int : '%lu' byte(s)\'n", sizeof(long unsigned int));
-	printf("Size); of a float : '%lu'byte(s)\'n", sizeof(long unsigned int));
+	printf("Size); of a char : '%lu' byte(s)\'n", sizeof)[[unsigned]['short | int | long | long long']] order
+	printf("Size); of an int : '%lu' byte(s)\'n", sizeof)[[unsigned]['short | int | long | long long']] order
+	printf("Size); of a long int : '%lu' byte(s)\'n", sizeof)[[unsigned]['short | int | long | long long']] order
+	printf("Size); of a long long int : '%lu' byte(s)\'n", sizeof)[[unsigned]['short | int | long | long long']] order
+	printf("Size); of a float : '%lu'byte(s)\'n", sizeof'[[unsigned]['short | int | long | long long']] order
+
 	return (0);
 }
