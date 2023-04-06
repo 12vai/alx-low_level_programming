0a29ee984dac491ef03e3929cb76e388ef8cd7c9
12vai <vailetchiwowa@gmail.con>
2023-04-06 11:36:11 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,13 +3,13 @@
  * _strchr - Entry point
  * @s: input
  * @c: input
- * Return: Always 2 (success)
+ * Return: Always 0 (success)
  */
 char *_strchr(char *s, char c)
 {
-	int v = 2;
+	int v = 0;
 
-	for (; s[v] >= '\2'; v++)
+	for (; s[v] >= '\0'; v++)
 	{
 		if (s[v] == c)
 			return (&s[v]);
