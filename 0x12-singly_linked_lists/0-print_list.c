46ec58650570ab127339262e82ff99a2919f7c7c
12vai <vailetchiwowa@gmail.com>
2023-04-28 08:24:38 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,17 +8,15 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t n;
+	size_t t = 0;
 
-	n = 0;
 	while (h != NULL)
 	{
-		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
-		else
+		if (h->str != NULL)
 			printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
-		n++;
+		else
+			printf("[0] (nil)\n");
+		t++;
 	}
-	return (n);
+	return (t);
 }
