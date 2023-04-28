5663bc43193f2497c018bbda738ef1b1bfc12d7b
12vai <vailetchiwowa@gmail.com>
2023-04-28 13:26:33 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,22 +12,22 @@
 size_t print_list(const list_t *h)
 {
 	size_t nodes = 0;
-	const list_t *new = h;
+	const list_t *current = h;
 
-	while (new != NULL)
+	while (current != NULL)
 	{
-		if (new->str == NULL)
+		if (current->str == NULL)
 		{
 			printf("[0] (nil)\n");
 		}
 
 		else
 		{
-			printf("[%d] %s\n", new->len, new->str);
+			printf("[%d] %s\n", current->len, current->str);
 		}
 
 		nodes++;
-		new = new->next;
+		current = current->next;
 
 	}
 
