2bcf399e914ee4d7655f33b1ddc0d75b05f60232
12vai <vailetchiwowa@gmail.com>
2023-05-02 07:37:10 -0700

    100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -8,18 +8,18 @@
  */
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *vio_node = NULL;
+	listint_t *prev_node = NULL;
 	listint_t *current_node = *head;
 	listint_t *next_node;
 
 	while (current_node != NULL)
 	{
 		next_node = current_node->next;
-		current_node->next = vio_node;
-		vio_node = current_node;
+		current_node->next = prev_node;
+		prev_node = current_node;
 		current_node = next_node;
 	}
 
-	*head = vio_node;
+	*head = prev_node;
 	return (*head);
 }
