94dece5ef3629a6299317c88a88be87350652845
12vai <vailetchiwowa@gmail.com>
2023-05-04 13:22:54 -0700

    vai

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * grt_bit - gets the value of a given index.
+ * @n: the bit.
+ * @index: the index to get the value at - index start at 0.
+ *
+ * Return: if an error occurs - -1.
+ * otherwise - the value of bit at index.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+	{
+		return (-1);
+	}
+
+	unsigned long int mask = 1UL << index;
+
+	if ((n & mask) != 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
