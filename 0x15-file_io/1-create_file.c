ff7b666c02525dd534e0678e76716755ba69533e
12vai <vailetchiwowa@gmail.com>
2023-08-07 14:23:51 -0700

    o/i

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,30 +9,24 @@
 
 int create_file(const char *filename, char *text_content)
 {
-if (filename == NULL)
-return (-1);
+	int fd, v, len = 0;
 
+	if (filename == NULL)
+		return (-1);
 
-int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-if (fd == -1)
-return (-1);
+	if (text_content != NULL)
+	{
+		for  (len = 0; text_content[len];)
+			len++;
+	}
 
-if (text_content != NULL)
-{
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	v = write(fd, text_content, len);
 
-ssize_t text_length = 0;
-while (text_content[text_length])
-text_length++;
+	if (fd == -1 || v == -1)
+		return (-1);
 
-ssize_t bytes_written = write(fd, text_content, text_length);
-if (bytes_written != text_length)
-{
-close(fd);
-return (-1);
-}
-}
+	close(fd);
 
-close(fd);
-
-return (1);
+	return (1);
 }
