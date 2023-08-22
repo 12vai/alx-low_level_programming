116960fdf3d998a62d55eaadd549fd62d10cf1bb
12vai <vailetchiwowa@gmail.com>
2023-08-22 07:33:26 -0700

    dy

---
+++ 0x18-dynamic_libraries/5-strstr.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring.
+ * @haystack: hello wold.
+ * @needle: world
+ * Return: Always 0 (success)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int k;
+
+	if (*needle == 0)
+		return (haystack);
+
+	while (*haystack)
+	{
+		k = 0;
+
+		if (haystack[k] == needle[k])
+		{
+			do {
+				if (needle[k + 1] == '\0')
+					return (haystack);
+
+				k++;
+			} while (haystack[k] == needle[k]);
+		}
+
+		haystack++;
+	}
+
+	return ('\0');
+}
